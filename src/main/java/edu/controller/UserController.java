3fd75b710cbe07cab55b5548588269727218afb8
bhanuka2 <bhanukashashiindra.com>
2026-04-21 23:35:06 +0530

    feat:implemented crud

--- src/main/java/edu/controller/UserController.java
+++ src/main/java/edu/controller/UserController.java
@@ -1,47 +1,27 @@
 package edu.controller;
 
+import edu.model.dto.UserDTO;
+import edu.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-public class UserController {
-
-
-}
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/admin")
+@RequestMapping("/api/user")
 @CrossOrigin
-public class AdminController {
+public class UserController {
 
+    private final UserService userService;
 
-    private final AdminService adminService;
+    @PostMapping("/addUser")
+    public void add(@RequestBody UserDTO userDTO){userService.add(userDTO);}
 
-    @GetMapping("/getAll")
-    public List<AdminDTO> getAll(AdminDTO adminDTO) {
-        return adminService.getAll(adminDTO);
-    }
-
-    @PostMapping("/addAdmin")
-    public void add(@RequestBody AdminDTO adminDTO) {
-        adminService.add(adminDTO);
-    }
     @PutMapping("/update{id}")
-    public void update(@RequestBody AdminDTO adminDTO) {
-        adminService.update(adminDTO);
-    }
+    public void update(@RequestBody UserDTO userDTO){userService.update(userDTO);}
 
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Long id ) {
-        adminService.delete(id);
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestBody AdminDTO adminDTO) {
-        return adminService.login(adminDTO);
-    }
+    public void delete(@PathVariable Long id){userService.delete(id);}
 
 
 }
+
--- src/main/java/edu/service/UserService.java
+++ src/main/java/edu/service/UserService.java
@@ -1,4 +1,11 @@
 package edu.service;
 
+import edu.model.dto.UserDTO;
+
 public interface UserService {
+    void add(UserDTO userDTO);
+
+    void update(UserDTO userDTO);
+
+    void delete(Long id);
 }
